e3ddd1b944c424d84c48445d99d22717a02bfcf9
Shaun Agostinho <shaunagostinho@gmail.com>
2025-10-09 14:11:43 -0700

    add create time on emit

--- workers/data.go
+++ workers/data.go
@@ -5,11 +5,13 @@ import (
 	"github.com/netwatcherio/netwatcher-agent/probes"
 	"github.com/netwatcherio/netwatcher-agent/web"
 	log "github.com/sirupsen/logrus"
+	"time"
 )
 
 func ProbeDataWorker(wsH *web.WSClient, ch chan probes.ProbeData) {
 	go func(cn *web.WSClient, c chan probes.ProbeData) {
 		for p := range ch {
+			p.CreatedAt = time.Now()
 			marshal, err := json.Marshal(p)
 			log.Warn(string(marshal))
 			if err != nil {
